ebac022094e0abc2b02f2f05d402220f1bc46668
Brion Vibber <brion@pobox.com>
2018-05-09 23:03:15 -0700

    refactoring

--- aotjs_runtime.cpp
+++ aotjs_runtime.cpp
@@ -1,132 +1,136 @@
 #include "aotjs_runtime.h"
 
-aotjs_type typeof_undefined = "undefined";
-aotjs_type typeof_number = "number";
-aotjs_type typeof_boolean = "boolean";
-aotjs_type typeof_string = "string";
-aotjs_type typeof_object = "object";
-
-#pragma mark aotjs_proplist
+namespace aotjs {
+  aotjs_type typeof_undefined = "undefined";
+  aotjs_type typeof_number = "number";
+  aotjs_type typeof_boolean = "boolean";
+  aotjs_type typeof_string = "string";
+  aotjs_type typeof_object = "object";
+}
 
 #pragma mark aotjs_ref hash helpers
 
 namespace std {
-  size_t hash<aotjs_ref>::operator()(aotjs_ref const& ref) const noexcept {
-    return std::hash<int64_t>{}(ref.raw());
+  size_t hash<::aotjs::aotjs_ref>::operator()(::aotjs::aotjs_ref const& ref) const noexcept {
+    return hash<int64_t>{}(ref.raw());
   }
 }
 
-bool aotjs_ref::operator==(const aotjs_ref &rhs) const {
-  // Bit-identical always matches!
-  // This catches matching pointers (same object), matching ints, etc.
-  if (raw() == rhs.raw()) {
-    return true;
-  }
+#pragma mark aotjs_proplist
 
-  if (is_object() && rhs.is_object()) {
-    auto obj_l = as_object();
-    auto type_l = obj_l->get_typeof();
-
-    auto obj_r = rhs.as_object();
-    auto type_r = obj_r->get_typeof();
-
-    if (type_l == typeof_string && type_r == typeof_string) {
-      // Two string instances may still be equal.
-      auto str_l = static_cast<aotjs_string *>(obj_l);
-      auto str_r = static_cast<aotjs_string *>(obj_r);
-      return (*str_l) == (*str_r);
+namespace aotjs {
+  bool aotjs_ref::operator==(const aotjs_ref &rhs) const {
+    // Bit-identical always matches!
+    // This catches matching pointers (same object), matching ints, etc.
+    if (raw() == rhs.raw()) {
+      return true;
     }
-  }
 
-  // Non-identical non-string objects never compare identical.
-  return false;
-}
+    if (is_object() && rhs.is_object()) {
+      auto obj_l = as_object();
+      auto type_l = obj_l->get_typeof();
 
-#pragma mark aotjs_object
+      auto obj_r = rhs.as_object();
+      auto type_r = obj_r->get_typeof();
 
-aotjs_ref aotjs_object::get_prop(aotjs_ref name) {
-  auto index = props.find(name);
-  if (index == props.end()) {
-    if (prototype) {
-      return prototype->get_prop(name);
-    } else {
-      return aotjs_undefined();
+      if (type_l == typeof_string && type_r == typeof_string) {
+        // Two string instances may still be equal.
+        auto str_l = static_cast<aotjs_string *>(obj_l);
+        auto str_r = static_cast<aotjs_string *>(obj_r);
+        return (*str_l) == (*str_r);
+      }
     }
-  } else {
-    return index->first;
-  }
-}
 
-void aotjs_object::set_prop(aotjs_ref name, aotjs_ref val) {
-  props.insert_or_assign(name, val);
-}
-
-aotjs_proplist aotjs_object::list_props() {
-  aotjs_proplist props = this;
-  return props;
-}
-
-#pragma mark aotjs_heap
-
-bool aotjs_heap::gc_get_mark(aotjs_object *obj) {
-  auto mark = marks.find(obj);
-  if (mark->first) {
-    return mark->second;
-  } else {
+    // Non-identical non-string objects never compare identical.
     return false;
   }
-}
 
-void aotjs_heap::gc_set_mark(aotjs_object *obj, bool val) {
-  marks.insert_or_assign(obj, val);
-}
+  #pragma mark aotjs_object
 
-void aotjs_heap::register_object(aotjs_object *obj) {
-  objects.insert(obj);
-  gc_set_mark(obj, false);
-}
+  aotjs_ref aotjs_object::get_prop(aotjs_ref name) {
+    auto index = props.find(name);
+    if (index == props.end()) {
+      if (prototype) {
+        return prototype->get_prop(name);
+      } else {
+        return aotjs_undefined();
+      }
+    } else {
+      return index->first;
+    }
+  }
 
-void aotjs_heap::gc_mark(aotjs_object *obj) {
-  if (!gc_get_mark(obj)) {
-    gc_set_mark(obj, true);
+  void aotjs_object::set_prop(aotjs_ref name, aotjs_ref val) {
+    props.insert_or_assign(name, val);
+  }
 
-    for (auto [prop_name, prop_val] : obj->list_props()) {
-      // prop names are always either strings or symbols, so objects.
-      gc_mark(prop_name.as_object());
+  aotjs_proplist aotjs_object::list_props() {
+    aotjs_proplist props = this;
+    return props;
+  }
 
-      // prop values may not be, so check!
-      if (prop_val.is_object()) {
-        gc_mark(prop_val.as_object());
+  #pragma mark aotjs_heap
+
+  bool aotjs_heap::gc_get_mark(aotjs_object *obj) {
+    auto mark = marks.find(obj);
+    if (mark->first) {
+      return mark->second;
+    } else {
+      return false;
+    }
+  }
+
+  void aotjs_heap::gc_set_mark(aotjs_object *obj, bool val) {
+    marks.insert_or_assign(obj, val);
+  }
+
+  void aotjs_heap::register_object(aotjs_object *obj) {
+    objects.insert(obj);
+    gc_set_mark(obj, false);
+  }
+
+  void aotjs_heap::gc_mark(aotjs_object *obj) {
+    if (!gc_get_mark(obj)) {
+      gc_set_mark(obj, true);
+
+      for (auto [prop_name, prop_val] : obj->list_props()) {
+        // prop names are always either strings or symbols, so objects.
+        gc_mark(prop_name.as_object());
+
+        // prop values may not be, so check!
+        if (prop_val.is_object()) {
+          gc_mark(prop_val.as_object());
+        }
       }
     }
   }
-}
 
-void aotjs_heap::gc_sweep(aotjs_object *obj) {
-  if (gc_get_mark(obj)) {
-    // Keep the object, but reset the marker value for next time.
-    gc_set_mark(obj, false);
-  } else {
-    // No findable references to this object. Destroy it!
-    objects.erase(obj);
-    marks.erase(obj);
-    delete obj;
-  }
-}
-
-void aotjs_heap::gc() {
-  gc_mark(root);
-
-  // Todo: search stack frames / held-live objects
-
-  // Todo: don't require allocating memory to free memory
-  std::vector<aotjs_object *> dead_objects;
-  for (auto obj : objects) {
+  void aotjs_heap::gc_sweep(aotjs_object *obj) {
     if (gc_get_mark(obj)) {
-      dead_objects.push_back(obj);
+      // Keep the object, but reset the marker value for next time.
+      gc_set_mark(obj, false);
+    } else {
+      // No findable references to this object. Destroy it!
+      objects.erase(obj);
+      marks.erase(obj);
+      delete obj;
     }
   }
-  for (auto obj : dead_objects) {
-    gc_sweep(obj);
+
+  void aotjs_heap::gc() {
+    gc_mark(root);
+
+    // Todo: search stack frames / held-live objects
+
+    // Todo: don't require allocating memory to free memory
+    std::vector<aotjs_object *> dead_objects;
+    for (auto obj : objects) {
+      if (gc_get_mark(obj)) {
+        dead_objects.push_back(obj);
+      }
+    }
+    for (auto obj : dead_objects) {
+      gc_sweep(obj);
+    }
   }
 }
--- aotjs_runtime.h
+++ aotjs_runtime.h
@@ -4,200 +4,205 @@
 #include <unordered_map>
 #include <unordered_set>
 
-typedef const char *aotjs_type;
+namespace aotjs {
+  typedef const char *aotjs_type;
 
-extern aotjs_type typeof_undefined;
-extern aotjs_type typeof_number;
-extern aotjs_type typeof_boolean;
-extern aotjs_type typeof_string;
-extern aotjs_type typeof_object;
+  extern aotjs_type typeof_undefined;
+  extern aotjs_type typeof_number;
+  extern aotjs_type typeof_boolean;
+  extern aotjs_type typeof_string;
+  extern aotjs_type typeof_object;
 
-class aotjs_object;
+  class aotjs_object;
 
-class aotjs_undefined {
-  // just a tag
-};
-
-class aotjs_ref {
-  union {
-    double val_double;
-    int64_t val_int64;
-    int32_t val_int32;
+  class aotjs_undefined {
+    // just a tag
   };
 
-public:
+  class aotjs_ref {
+    union {
+      double val_double;
+      int64_t val_int64;
+      int32_t val_int32;
+    };
 
-  // 13 bits reserved at top for NaN
-  // 1 bit to mark whether it's a tag
-  // 2 bits to mark subtype
-  // 48 bits at the bottom for pointers (for native x86_64)
-  // 16 bits apare + 32 bits at the bottom for ints
-  static const int64_t tag_mask      = 0xffff000000000000LL;
-  static const int64_t tag_double    = 0xfff8000000000000LL;
-  static const int64_t tag_int32     = 0xfffc000000000000LL;
-  static const int64_t tag_object    = 0xfffd000000000000LL;
-  static const int64_t tag_bool      = 0xfffe000000000000LL;
-  static const int64_t tag_undefined = 0xffff000000000000LL;
+  public:
 
-  aotjs_ref(double val) : val_double(val) {}
-  aotjs_ref(int32_t val) : val_int64(((int64_t)val & ~tag_mask) | tag_int32) {}
-  aotjs_ref(bool val) : val_int64(((int64_t)val & ~tag_mask) | tag_bool) {}
-  aotjs_ref(aotjs_object *val) : val_int64((reinterpret_cast<int64_t>(val) & ~tag_mask) | tag_bool) {}
-  aotjs_ref(aotjs_undefined val) : val_int64(tag_undefined) {}
+    // 13 bits reserved at top for NaN
+    // 1 bit to mark whether it's a tag
+    // 2 bits to mark subtype
+    // 48 bits at the bottom for pointers (for native x86_64)
+    // 16 bits apare + 32 bits at the bottom for ints
+    static const int64_t tag_mask      = 0xffff000000000000LL;
+    static const int64_t tag_double    = 0xfff8000000000000LL;
+    static const int64_t tag_int32     = 0xfffc000000000000LL;
+    static const int64_t tag_object    = 0xfffd000000000000LL;
+    static const int64_t tag_bool      = 0xfffe000000000000LL;
+    static const int64_t tag_undefined = 0xffff000000000000LL;
 
-  int64_t raw() const {
-    return val_int64;
-  }
+    aotjs_ref(double val) : val_double(val) {}
+    aotjs_ref(int32_t val) : val_int64(((int64_t)val & ~tag_mask) | tag_int32) {}
+    aotjs_ref(bool val) : val_int64(((int64_t)val & ~tag_mask) | tag_bool) {}
+    aotjs_ref(aotjs_object *val) : val_int64((reinterpret_cast<int64_t>(val) & ~tag_mask) | tag_bool) {}
+    aotjs_ref(aotjs_undefined val) : val_int64(tag_undefined) {}
 
-  int64_t tag() const {
-    return val_int64 & tag_mask;
-  }
+    int64_t raw() const {
+      return val_int64;
+    }
 
-  bool is_double() const {
-    return tag() == tag_double;
-  }
+    int64_t tag() const {
+      return val_int64 & tag_mask;
+    }
 
-  bool is_int32() const {
-    return tag() == tag_int32;
-  }
+    bool is_double() const {
+      return tag() == tag_double;
+    }
 
-  bool is_object() const {
-    return tag() == tag_object;
-  }
+    bool is_int32() const {
+      return tag() == tag_int32;
+    }
 
-  bool is_bool() const {
-    return tag() == tag_bool;
-  }
+    bool is_object() const {
+      return tag() == tag_object;
+    }
 
-  bool is_undefined() const {
-    return tag() == tag_undefined;
-  }
+    bool is_bool() const {
+      return tag() == tag_bool;
+    }
 
-  double as_double() const {
-    // Interpret all bits as double-precision float
-    return val_double;
-  }
+    bool is_undefined() const {
+      return tag() == tag_undefined;
+    }
 
-  int32_t as_int32() const {
-    // Bottom 32 bits are ours for ints.
-    return val_int32;
-  }
+    double as_double() const {
+      // Interpret all bits as double-precision float
+      return val_double;
+    }
 
-  bool as_bool() const {
-    // Bottom 1 bit is all we need!
-    // But treat it like an int32.
-    return (bool)val_int32;
-  }
+    int32_t as_int32() const {
+      // Bottom 32 bits are ours for ints.
+      return val_int32;
+    }
 
-  aotjs_object *as_object() const {
-#if (PTRDIFF_MAX) > 2147483647
-  // 64-bit host -- drop the top 16 bits of NaN and tag.
-  // Assumes address space has only 48 significant bits
-  // but may be signed, as on x86_64.
-  return reinterpret_cast<aotjs_object *>((val_int64 << 16) >> 16);
-#else
-  // 32 bit host -- bottom bits are ours, like an int.
-  return reinterpret_cast<aotjs_object *>(val_int32);
-#endif
-  }
+    bool as_bool() const {
+      // Bottom 1 bit is all we need!
+      // But treat it like an int32.
+      return (bool)val_int32;
+    }
 
-  aotjs_undefined as_undefined() const {
-    aotjs_undefined undef;
-    return undef;
-  }
+    aotjs_object *as_object() const {
+  #if (PTRDIFF_MAX) > 2147483647
+    // 64-bit host -- drop the top 16 bits of NaN and tag.
+    // Assumes address space has only 48 significant bits
+    // but may be signed, as on x86_64.
+    return reinterpret_cast<aotjs_object *>((val_int64 << 16) >> 16);
+  #else
+    // 32 bit host -- bottom bits are ours, like an int.
+    return reinterpret_cast<aotjs_object *>(val_int32);
+  #endif
+    }
 
-  bool operator==(const aotjs_ref &rhs) const;
-};
+    aotjs_undefined as_undefined() const {
+      aotjs_undefined undef;
+      return undef;
+    }
+
+    bool operator==(const aotjs_ref &rhs) const;
+  };
+
+}
 
 namespace std {
-  template<> struct hash<aotjs_ref> {
-      size_t operator()(aotjs_ref const& ref) const noexcept;
+  template<> struct hash<::aotjs::aotjs_ref> {
+      size_t operator()(::aotjs::aotjs_ref const& ref) const noexcept;
   };
 }
 
-class aotjs_proplist;
+namespace aotjs {
+  class aotjs_proplist;
 
-class aotjs_object {
-  aotjs_type typeof;
-  aotjs_object *prototype;
-  std::unordered_map<aotjs_ref,aotjs_ref> props;
+  class aotjs_object {
+    aotjs_type typeof;
+    aotjs_object *prototype;
+    ::std::unordered_map<aotjs_ref,aotjs_ref> props;
 
-  friend class aotjs_proplist;
+    friend class aotjs_proplist;
 
-public:
+  public:
 
-  aotjs_type get_typeof() const {
-    return typeof;
-  }
+    aotjs_type get_typeof() const {
+      return typeof;
+    }
 
-  aotjs_ref get_prop(aotjs_ref name);
-  void set_prop(aotjs_ref name, aotjs_ref val);
-  aotjs_proplist list_props();
-};
+    aotjs_ref get_prop(aotjs_ref name);
+    void set_prop(aotjs_ref name, aotjs_ref val);
+    aotjs_proplist list_props();
+  };
 
-class aotjs_proplist {
-  aotjs_object *obj;
+  class aotjs_proplist {
+    aotjs_object *obj;
 
-  aotjs_proplist(aotjs_object *obj)
-  : obj(obj) {
-    //
-  }
+    aotjs_proplist(aotjs_object *obj)
+    : obj(obj) {
+      //
+    }
 
-friend class aotjs_object;
+  friend class aotjs_object;
 
-public:
+  public:
 
-  std::unordered_map<aotjs_ref,aotjs_ref>::iterator begin() noexcept {
-    return obj->props.begin();
-  }
+    ::std::unordered_map<aotjs_ref,aotjs_ref>::iterator begin() noexcept {
+      return obj->props.begin();
+    }
 
-  std::unordered_map<aotjs_ref,aotjs_ref>::const_iterator cbegin() const noexcept {
-    return obj->props.cbegin();
-  }
+    ::std::unordered_map<aotjs_ref,aotjs_ref>::const_iterator cbegin() const noexcept {
+      return obj->props.cbegin();
+    }
 
-  std::unordered_map<aotjs_ref,aotjs_ref>::iterator end() noexcept {
-    return obj->props.end();
-  }
+    ::std::unordered_map<aotjs_ref,aotjs_ref>::iterator end() noexcept {
+      return obj->props.end();
+    }
 
-  std::unordered_map<aotjs_ref,aotjs_ref>::const_iterator cend() const noexcept {
-    return obj->props.cend();
-  }
-};
+    ::std::unordered_map<aotjs_ref,aotjs_ref>::const_iterator cend() const noexcept {
+      return obj->props.cend();
+    }
+  };
 
-class aotjs_string : public aotjs_object {
-  std::wstring data;
+  class aotjs_string : public aotjs_object {
+    ::std::wstring data;
 
-public:
-  aotjs_string(std::wstring const &src)
-  : data(src) {
-    //
-  }
+  public:
+    aotjs_string(::std::wstring const &src)
+    : data(src) {
+      //
+    }
 
-  operator std::wstring() const {
-    return data;
-  }
+    operator ::std::wstring() const {
+      return data;
+    }
 
-  bool operator==(const aotjs_string &rhs) const {
-    return data == rhs.data;
-  }
-};
+    bool operator==(const aotjs_string &rhs) const {
+      return data == rhs.data;
+    }
+  };
 
-class aotjs_heap {
-  aotjs_object *root;
-  std::unordered_set<aotjs_object *> objects;
-  std::unordered_map<aotjs_object *, bool> marks;
+  class aotjs_heap {
+    aotjs_object *root;
+    ::std::unordered_set<aotjs_object *> objects;
+    ::std::unordered_map<aotjs_object *, bool> marks;
 
-  bool gc_get_mark(aotjs_object *obj);
-  void gc_set_mark(aotjs_object *obj, bool val);
-  void gc_mark(aotjs_object *obj);
-  void gc_sweep(aotjs_object *obj);
+    bool gc_get_mark(aotjs_object *obj);
+    void gc_set_mark(aotjs_object *obj, bool val);
+    void gc_mark(aotjs_object *obj);
+    void gc_sweep(aotjs_object *obj);
 
-public:
-  aotjs_heap(aotjs_object *aRoot)
-  : root(aRoot) {
-    register_object(root);
-  }
+  public:
+    aotjs_heap(aotjs_object *aRoot)
+    : root(aRoot) {
+      register_object(root);
+    }
 
-  void register_object(aotjs_object *obj);
-  void gc();
-};
+    void register_object(aotjs_object *obj);
+    void gc();
+  };
+}
