8fa8713d73e9748ae095a2104a057686b1ecf009
Brion Vibber <brion@pobox.com>
2018-05-12 08:10:44 -0700

    use lambdas for shits and giggles

--- samples/closure.cpp
+++ samples/closure.cpp
@@ -4,64 +4,6 @@
 
 using namespace AotJS;
 
-Val work_body(Engine *engine, Function *func, Frame *frame);
-Val func_body(Engine *engine, Function *func, Frame *frame);
-
-Val work_body(Engine *aEngine, Function *aFunc, Frame *aFrame) {
-  // Variable hoisting!
-  // Conceptually we allocate all the locals at the start of the scope.
-  // They'll all be filled with the JS `undefined` value initially.
-  //
-  // We allocate them on the stack frame if they're not captured, but
-  // note we allocate a lexical scope first for those which are captured
-  // by the closure function. That scope will live on separately after
-  // the current function ends.
-  Scope* const _scope1 = new Scope(aEngine, aFunc->scope(), 1);
-  *(aFrame->local(0)) = _scope1;
-  Val* const b = _scope1->local(0);
-
-  Val* const a = aFrame->local(1);
-  Val* const func = aFrame->local(2);
-  Val* const anon_retval = aFrame->local(3);
-
-  // function declarations happen at the top of the scope too.
-  // This is where we capture the `b` variable's location, knowing
-  // its actual value can change.
-  *func = new Function(aEngine,
-    func_body, // implementation
-    "func",    // name
-    0,         // arg arity
-    0,         // locals
-    _scope1,   // lexical scope
-    {b}        // captures
-  );
-
-  // Now we get to the body of the function:
-  *a = new String(aEngine, "a");
-  *b = new String(aEngine, "b");
-
-  std::cout << "should say 'b': " << b->dump() << "\n";
-
-  // Make the call!
-  *anon_retval = aEngine->call(*func, Null(), {});
-
-  // should say "b plus one"
-  std::cout << "should say 'b plus one': " << b->dump() << "\n";
-
-  return Undefined();
-}
-
-// The closure function...
-Val func_body(Engine *engine, Function *func, Frame *frame) {
-  // Unlike the locals and args arrays, captures gives you a ptr to a ptr
-  Val* const b = func->capture(0);
-
-  // replace the variable in the parent scope
-  *b = new String(engine, "b plus one");
-
-  return Undefined();
-}
-
 int main() {
   AotJS::Engine engine;
 
@@ -71,7 +13,64 @@ int main() {
   // Register the function!
   auto work = new Function(
     &engine,
-    work_body,
+    // Use a lambda for source prettiness.
+    // Must be no C++ captures so we can turn it into a raw function pointer!
+    [] (Engine *aEngine, Function *aFunc, Frame *aFrame) -> Val {
+      // Variable hoisting!
+      // Conceptually we allocate all the locals at the start of the scope.
+      // They'll all be filled with the JS `undefined` value initially.
+      //
+      // We allocate them on the stack frame if they're not captured, but
+      // note we allocate a lexical scope first for those which are captured
+      // by the closure function. That scope will live on separately after
+      // the current function ends.
+      Scope* const _scope1 = new Scope(aEngine, aFunc->scope(), 1);
+      *(aFrame->local(0)) = _scope1;
+      Val* const b = _scope1->local(0);
+
+      Val* const a = aFrame->local(1);
+      Val* const func = aFrame->local(2);
+      Val* const anon_retval = aFrame->local(3);
+
+      // function declarations happen at the top of the scope too.
+      // This is where we capture the `b` variable's location, knowing
+      // its actual value can change.
+      *func = new Function(aEngine,
+        // implementation
+        [] (Engine *engine, Function *func, Frame *frame) -> Val {
+          // Note we cannot use C++'s captures here -- they're not on GC heap and
+          // would turn our call reference into a fat pointer, which we don't want.
+          //
+          // The capture gives you a pointer into one of the linked Scopes,
+          // which are retained via a chain referenced by the Function object.
+          Val* const b = func->capture(0);
+
+          // replace the variable in the parent scope
+          *b = new String(engine, "b plus one");
+
+          return Undefined();
+        },
+        "func",    // name
+        0,         // arg arity
+        0,         // locals
+        _scope1,   // lexical scope
+        {b}        // captures
+      );
+
+      // Now we get to the body of the function:
+      *a = new String(aEngine, "a");
+      *b = new String(aEngine, "b");
+
+      std::cout << "should say 'b': " << b->dump() << "\n";
+
+      // Make the call!
+      *anon_retval = aEngine->call(*func, Null(), {});
+
+      // should say "b plus one"
+      std::cout << "should say 'b plus one': " << b->dump() << "\n";
+
+      return Undefined();
+    },
     "work",
     1,        // argument count
     5,        // locals count
--- samples/gc.cpp
+++ samples/gc.cpp
@@ -4,38 +4,6 @@
 
 using namespace AotJS;
 
-Val work_body(Engine *engine, Function *func, Frame *frame) {
-  // Fetch the argument list. The function arity must be correct!
-  // Attempting to read beyond the actual number will be invalid.
-  // Can skip this call if no references to args.
-  auto root = frame->arg(0);
-
-  auto obj = frame->local(0);
-  auto objname = frame->local(1);
-  auto propname = frame->local(2);
-  auto propval = frame->local(3);
-  auto unused = frame->local(4);
-  auto notpropname = frame->local(5);
-
-  *obj = new Object(engine, nullptr);
-  *objname = new String(engine, "an_obj");
-  *propname = new String(engine, "propname");
-  *propval = new String(engine, "propval");
-  *unused = new String(engine, "unused");
-
-  // This string is a different instance from the earlier "propname" one,
-  // and should not survive GC.
-  *notpropname = new String(engine, std::string("prop") + std::string("name"));
-
-  // Retain a couple strings on an object
-  // @todo this would be done with a wrapper interface probably
-  obj->asObject()->setProp(*propname, *propval);
-
-  root->asObject()->setProp(*objname, *obj);
-
-  return Undefined();
-}
-
 int main() {
   Engine engine;
 
@@ -45,7 +13,37 @@ int main() {
   // Register the function!
   auto func = new Function(
     &engine,
-    work_body,
+    [] (Engine *engine, Function *func, Frame *frame) -> Val {
+      // Fetch the argument list. The function arity must be correct!
+      // Attempting to read beyond the actual number will be invalid.
+      // Can skip this call if no references to args.
+      auto root = frame->arg(0);
+
+      auto obj = frame->local(0);
+      auto objname = frame->local(1);
+      auto propname = frame->local(2);
+      auto propval = frame->local(3);
+      auto unused = frame->local(4);
+      auto notpropname = frame->local(5);
+
+      *obj = new Object(engine, nullptr);
+      *objname = new String(engine, "an_obj");
+      *propname = new String(engine, "propname");
+      *propval = new String(engine, "propval");
+      *unused = new String(engine, "unused");
+
+      // This string is a different instance from the earlier "propname" one,
+      // and should not survive GC.
+      *notpropname = new String(engine, std::string("prop") + std::string("name"));
+
+      // Retain a couple strings on an object
+      // @todo this would be done with a wrapper interface probably
+      obj->asObject()->setProp(*propname, *propval);
+
+      root->asObject()->setProp(*objname, *obj);
+
+      return Undefined();
+    },
     "work",
     1, // argument count
     5, // number of locals
